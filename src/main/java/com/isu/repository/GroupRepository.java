5e7675d7fc37a3c5483c144dac061cd86659bdd6
Stas Belov <sstasbelov@gmail.com>
2019-10-27 12:49:45 +0300

    revert lesha commit

--- src/main/java/com/isu/repository/GroupRepository.java
+++ src/main/java/com/isu/repository/GroupRepository.java
@@ -2,12 +2,13 @@ package com.isu.repository;
 
 import com.isu.model.Group;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface GroupRepository extends CrudRepository<Group, Long> {
 
     List<Group> findAll();
-
 }
 
