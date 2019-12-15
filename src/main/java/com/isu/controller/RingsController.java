2fae6503cf22a0a5f9bf92b24741ff044b5f46f0
Alexey Pulich <alexpulich@gmail.com>
2019-12-15 22:58:13 +0300

    анализ кольца

--- src/main/java/com/isu/controller/RingsController.java
+++ src/main/java/com/isu/controller/RingsController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -23,13 +24,28 @@ public class RingsController {
     private IUserService userService;
 
 
-    @RequestMapping(value = "/{ringId}", method = RequestMethod.GET)
-    public ModelAndView get(@PathVariable("ringId") long ringId) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView get(@RequestParam(name = "ringId", required = false) String ringId) {
         ModelAndView modelAndView = new ModelAndView();
 
-        List<User> users = userService.findByRing(ringService.findById(ringId));
+        List<User> users;
+        String error = null;
+
+        if (ringId != null) {
+            try {
+                long ringIdLong = Long.parseLong(ringId);
+                users = userService.findByRing(ringService.findById(ringIdLong));
+            } catch (NumberFormatException exception) {
+                users = null;
+                error = "Некорректный ID кольца!";
+            }
+        } else {
+            users = null;
+        }
 
         modelAndView.addObject("users", users);
+        modelAndView.addObject("error", error);
+        modelAndView.addObject("ringId", ringId);
         modelAndView.setViewName("private/rings/detail");
         return modelAndView;
     }
