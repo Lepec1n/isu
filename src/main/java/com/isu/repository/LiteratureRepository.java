76efb40794f6c7b5cd8680c40450fa31965f648f
Stas Belov <sstasbelov@gmail.com>
2019-12-16 12:23:50 +0300

    add unit test

--- src/main/java/com/isu/repository/LiteratureRepository.java
+++ src/main/java/com/isu/repository/LiteratureRepository.java
@@ -2,6 +2,7 @@ package com.isu.repository;
 
 import com.isu.model.Literature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
